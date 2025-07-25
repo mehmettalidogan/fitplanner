254411f43934267cea10e5480f0fa465f7037381
mehmettalidogan <ledogan80@gmail.com>
2025-07-25 12:00:30 +0300

    fix: Remove auto-fill for admin login credentials

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -14,8 +14,8 @@ const Login: React.FC = () => {
 
   const handleAdminLogin = () => {
     setIsAdminMode(true);
-    setEmail('ledogan80@gmail.com');
-    setPassword('805680mad');
+    setEmail('');
+    setPassword('');
     setError('');
   };
 
@@ -124,16 +124,6 @@ const Login: React.FC = () => {
               : 'Sağlıklı yaşam yolculuğunuza devam etmek için giriş yapın'
             }
           </p>
-          {isAdminMode && (
-            <div className="mt-3 p-3 bg-red-100 border border-red-200 rounded-lg">
-              <p className="text-xs text-red-600 text-center flex items-center justify-center">
-                <svg className="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                </svg>
-                Admin bilgileri otomatik dolduruldu
-              </p>
-            </div>
-          )}
         </div>
 
         {error && (
